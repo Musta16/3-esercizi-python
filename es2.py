538962bb1f92a20e5586de4c84bffc46756610e7
Musta16 <mostafaaboukelila@gmail.com>
2022-03-21 17:53:02 +0100

    - inserted three dictionarys, only two have keys-values - inserted the first FOR to check if there are any same keys in diz1 and diz2 - inserted the second FOR to check if there are any other key left out to put in diz3

---
+++ es2.py
@@ -0,0 +1,18 @@
+# realizzare un programma che dati due dizionari ne crei un terzo in cui i valori delle chiavi in comune siano fusi in
+# una lista
+
+diz1 = {'marca': 'Fiat', 'anno': 2005}
+diz2 = {'marca': 'Opel', 'proprietario': 'Musta'}
+diz3 = {}
+
+for chiave1 in diz1: # ciclo ogni chiave dentro il primo diz
+    if chiave1 in diz2: #controllo se la chiave è presente anche nel secondo diz
+        diz3[chiave1] = [diz1[chiave1], diz2[chiave1]] #se sì, aggiungo i valori dentro una list nel diz3
+    else:
+        diz3[chiave1] = diz1[chiave1] #se no, aggiungo la coppia chiave-valore nel diz3
+
+for chiave2 in diz2: #ciclo ogni chiave dentro il secondo diz
+    if chiave2 not in diz1: #controllo se non è presente nel primo diz
+        diz3[chiave2] = diz2[chiave2] #aggiungo come chiave-valore dentro il diz3
+
+print(diz3)
